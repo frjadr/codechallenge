0ae3017511682d5fd81cf052339acfa78d00cde1
DESKTOP-M9D3BKM\admin1 <frjadrijev@outlook.com>
2023-03-28 17:06:42 +0200

    ExportMapperHelperUnitTests extended

--- tests/PowerServiceReporting.ApplicationCore.UnitTests/Helpers/ExportMapperHelperUnitTests.cs
+++ tests/PowerServiceReporting.ApplicationCore.UnitTests/Helpers/ExportMapperHelperUnitTests.cs
@@ -14,37 +14,17 @@ namespace PowerServiceReporting.ApplicationCore.UnitTests.Helpers
     {
         private const string TimeZoneId = "GMT Standard Time";
         private readonly DateTime _clientLocalTime;
+        private List<PowerTradeDTO> powerTrades;
 
-        public ExportMapperHelperUnitTests() => _clientLocalTime = TimeZoneId.LocalClientTime();
-       
+        public ExportMapperHelperUnitTests()
+        {
+            _clientLocalTime = TimeZoneId.LocalClientTime();
+            powerTrades = InitializePowerTradeDTOs(_clientLocalTime);
+        }
+
         [Fact]
         public void MapPowerTradesToPowerTradesExportAggregated_ShouldReturn_AggregatedData_WithCorrectSumAndPositioning()
         {
-            // Arrange
-            var powerTrades = new List<PowerTradeDTO>
-            {
-                new PowerTradeDTO
-                {
-                    Date = _clientLocalTime,
-                    Periods = new PowerPeriodDTO[]
-                    {
-                        new PowerPeriodDTO() { Period = 1, Volume = 23.5512 },
-                        new PowerPeriodDTO() { Period = 0, Volume = 32.1255 },
-                        new PowerPeriodDTO() { Period = 24, Volume = 12.5532 }
-                    }
-                },
-                new PowerTradeDTO
-                {
-                    Date = _clientLocalTime,
-                    Periods = new PowerPeriodDTO[]
-                    {
-                        new PowerPeriodDTO() { Period = 1, Volume = 12.5532 },
-                        new PowerPeriodDTO() { Period = 0, Volume = 32.1255 },
-                        new PowerPeriodDTO() { Period = 24, Volume = 23.5512 }
-                    }
-                }
-            };
-
             var result = ExportMapperHelper.MapPowerTradesToPowerTradesExportAggregated(powerTrades, _clientLocalTime);
 
             Assert.Equal("23:00", result[0].Period); // should end with 23:00 period
@@ -55,6 +35,48 @@ namespace PowerServiceReporting.ApplicationCore.UnitTests.Helpers
             Assert.Equal(23.5512 + 12.5532, result[0].Volume);  
             Assert.Equal(32.1255 + 32.1255, result[1].Volume);
             Assert.Equal(12.5532 + 23.5512, result[2].Volume);
+        }
+
+        [Fact]
+        public void MapPowerTradesToPowerTradesExportNonAggregated_ShouldReturn_NonAggregatedData_WithCorrectPositioning()
+        {
+            var result = ExportMapperHelper.MapPowerTradesToPowerTradesExportNonAggregated(powerTrades, _clientLocalTime);
+
+            Assert.Equal("23:00", result[0].Period); // should end with 23:00 period
+            Assert.Equal("23:00", result[1].Period); // should end with 23:00 period
+            Assert.Equal("00:00", result[2].Period); // should start with 00:00 period
+            Assert.Equal("00:00", result[3].Period); // should start with 00:00 period
+            Assert.Equal("22:00", result[4].Period); // should start with 22:00 period
+            Assert.Equal("22:00", result[5].Period); // should start with 22:00 period
+        }
+
+        private List<PowerTradeDTO> InitializePowerTradeDTOs(DateTime clientLocalTime)
+        {
+            var powerTrades = new List<PowerTradeDTO>
+            {
+                new PowerTradeDTO
+                {
+                    Date = clientLocalTime,
+                    Periods = new PowerPeriodDTO[]
+                    {
+                        new PowerPeriodDTO() { Period = 1, Volume = 23.5512 },
+                        new PowerPeriodDTO() { Period = 0, Volume = 32.1255 },
+                        new PowerPeriodDTO() { Period = 24, Volume = 12.5532 }
+                    }
+                },
+                new PowerTradeDTO
+                {
+                    Date = clientLocalTime,
+                    Periods = new PowerPeriodDTO[]
+                    {
+                        new PowerPeriodDTO() { Period = 1, Volume = 12.5532 },
+                        new PowerPeriodDTO() { Period = 0, Volume = 32.1255 },
+                        new PowerPeriodDTO() { Period = 24, Volume = 23.5512 }
+                    }
+                }
+            };
+
+            return powerTrades;
         }
     }
 }
